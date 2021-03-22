dba9ec4fe33be1278ce7c3b92153100792e8ed87
Bekzod <9userr@gmail.com>
2021-03-22 22:15:35 +0300

    Register PostgreSQL in settings.py

--- coders_hub/settings.py
+++ coders_hub/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'coders_hub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coders_hub',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

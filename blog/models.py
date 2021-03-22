f44f4b0cdc81e7800d911305d000014d64eedd08
Bekzod <9userr@gmail.com>
2021-03-22 23:20:44 +0300

    Create migration for posts table

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    pass
+class posts(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user_id = models.ForeignKey(users,db_column='id',on_delete=models.CASCADE)
+    name = models.CharField()
+    desc = models.TextField()
+    img = models.CharField()
+    excerpt = models.CharField()
+    created_at = models.TimeField()
+    updated_at = models.TimeField()

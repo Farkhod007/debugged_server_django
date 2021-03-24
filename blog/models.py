e1eca3ca748be0112694b0523be5185e1367c2a5
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-03-25 00:42:29 +0500

    Delete post User model and use auth User instead

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    pass
-
-    def __str__(self):
-        return self.name
      
-
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     name = models.CharField(max_length = 255)

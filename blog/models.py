12042eb007434bb174e7c7a37bf2caaf0523fdf6
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-03-25 00:26:54 +0500

    Update blog models.py

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class User(models.Model):
     pass
 
+    def __str__(self):
+        return self.name
+     
+
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     name = models.CharField(max_length = 255)
@@ -12,3 +16,7 @@ class Post(models.Model):
     excerpt = models.TextField()
     created_at = models.TimeField()
     updated_at = models.TimeField()
+
+    def __str__(self):
+        return self.name
+    

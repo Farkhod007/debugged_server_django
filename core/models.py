6f53dbaf9c4abd6ef00e70f88f9b2227eccb4488
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-03-25 02:10:11 +0500

    Refactor blog app

--- core/models.py
+++ core/models.py
@@ -3,13 +3,14 @@ from django.db import models
      
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
-    name = models.CharField(max_length = 255)
-    desc = models.TextField() 
-    img = models.CharField(max_length = 255)
+    title = models.CharField(max_length = 255)
+    body = models.TextField() 
+    image = models.ImageField(upload_to = 'posts') 
     excerpt = models.TextField()
     created_at = models.TimeField()
     updated_at = models.TimeField()
+    deleted_at = models.TimeField(default = None, blank = True, null = True)
 
     def __str__(self):
-        return self.name
+        return self.title
     

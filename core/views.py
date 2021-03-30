f684ea1cca4e5bf60fc592d338e6a8c51f27a871
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-03-30 09:18:24 +0500

    Fix conflict in urls and view

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.templatetags.static import static
-def index(request):
+
+def home(request):
     posts = {
         'title': 'Lorem Ipsum', 
         'image': static('images/nature.jpeg'), 
         'desc': 'The default configuration is purposefully kept to a minimum.The Jinja2 backend adds the globals request, csrf_input, and csrf_token to the context.'
     }
-    return render(request, 'core/index.html', {'posts': posts})
+    return render(request, 'core/home.html', {'posts': posts})
     

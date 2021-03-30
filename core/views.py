b89d3b559f77b4955447a54703306bde429ca4b2
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-03-30 10:13:01 +0500

    Refactor

--- core/views.py
+++ core/views.py
@@ -2,10 +2,27 @@ from django.shortcuts import render
 from django.templatetags.static import static
 
 def home(request):
-    posts = {
-        'title': 'Lorem Ipsum', 
-        'image': static('images/nature.jpeg'), 
-        'desc': 'The default configuration is purposefully kept to a minimum.The Jinja2 backend adds the globals request, csrf_input, and csrf_token to the context.'
-    }
+    posts = [
+        {
+            'title': 'Lorem Ipsum', 
+            'image': static('images/nature.jpeg'), 
+            'desc': 'The default configuration is purposefully kept to a minimum.The Jinja2 backend adds the globals request, csrf_input, and csrf_token to the context.'
+        },
+        {
+            'title': 'Lorem Ipsum', 
+            'image': static('images/nature.jpeg'), 
+            'desc': 'The default configuration is purposefully kept to a minimum.The Jinja2 backend adds the globals request, csrf_input, and csrf_token to the context.'
+        },
+        {
+            'title': 'Lorem Ipsum', 
+            'image': static('images/nature.jpeg'), 
+            'desc': 'The default configuration is purposefully kept to a minimum.The Jinja2 backend adds the globals request, csrf_input, and csrf_token to the context.'
+        },
+        {
+            'title': 'Lorem Ipsum', 
+            'image': static('images/nature.jpeg'), 
+            'desc': 'The default configuration is purposefully kept to a minimum.The Jinja2 backend adds the globals request, csrf_input, and csrf_token to the context.'
+        }
+    ]
     return render(request, 'core/home.html', {'posts': posts})
     

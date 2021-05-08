d41343f26fda43b16c5338a4e1130227785ab0c8
bekjon <bekjonbegaliyev4@gmail.com>
2021-05-08 17:04:05 +0500

    Populate-post-detail-template-with-db-records

--- core/views.py
+++ core/views.py
@@ -2,17 +2,43 @@ from django.shortcuts import render
 from django.templatetags.static import static
 from core.models.post import Post
 from core.models.category import Category
+from django.utils import timezone
+from django.http import Http404
+
+def posts(request, id):
+    post = Post.objects.get(pk = id)
+    
+    return render(request, 'core/post_detail.html', {'post': post})
 
 def home(request):
-    feturedPost = Post.objects.get(featured = True)
-    regularPosts = Post.objects.filter(featured = False)
-    return render(request, 'core/home.html', {
-        'feturedPost': feturedPost,
+
+    try:
+        featuredPost = Post.objects.filter(
+            status = 'PB',
+            featured = True,
+            created_at__lt = timezone.now(),
+        ).order_by('-created_at')[0]
+    except IndexError:
+        featuredPost = None
+
+    regularPosts = Post.objects.filter(
+        status = 'PB',
+        featured = False,
+        created_at__lt = timezone.now()
+    ).order_by('-created_at')
+
+    context = {
+        'featuredPost': featuredPost,
         'regularPosts': regularPosts
-    })
+    }
+
+    return render(request, 'core/home.html', context)
+ 
 
 def category(request, id):
-    posts = Category.objects.get(pk = id).posts.all
-    return render(request, 'core/categories.html', {
-        'posts': posts
-    })
+
+    context = {
+        'posts': Category.objects.get(pk = id).posts.all
+    }
+
+    return render(request, 'core/categories.html', context)
